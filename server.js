61227d2ca177309a0aa343c0d460a0e02c3c27b9
Ezzy45 <esbbeavogui4@gmail.com>
2025-07-04 07:48:11 +0200

    Update server.js

--- server.js
+++ server.js
@@ -305,7 +305,7 @@ app.post('/submit-order', (req, res) => {
   secure: false, // true si port 465
   auth: {
     user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS+
+    pass: process.env.EMAIL_PASS
     }
   });
   
